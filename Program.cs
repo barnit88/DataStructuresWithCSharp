ded1c8559fd84424be0130c2729e2be2ce48abca
Barnit Basnet <barnit.basnet1@gmail.com>
2022-03-20 23:53:55 +0545

    Implementing Dynamic Array

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Arrays;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new Array<int>();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+            array.Add(5);
+            array.Add(6);
+            array.Add(7);
+            array.Add(8);
+            array.Add(9);
+            array.Add(10);
+            Console.WriteLine(array.Size());
+            array.RemoveAt(4);
+            Console.WriteLine(array.Size());
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
