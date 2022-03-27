5c3e288704a50934c39168a0e034f40034e3050a
Barnit Basnet <barnit.basnet1@gmail.com>
2022-03-27 21:29:38 +0545

    Heap |  Min Heap And Max Heap | WIP

--- DataStructure/Heap.cs
+++ DataStructure/Heap.cs
@@ -1,5 +1,8 @@
 ﻿using LinkedList;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BinaryHeap
 {
@@ -24,8 +27,172 @@ namespace BinaryHeap
     ///            5    4 3    2
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class Heap<T>
+    public class MinHeap<T> : Heap<T> where T : IComparable
     {
+        /// <summary>
+        /// Returns Capacity of Heap
+        /// </summary>
+        public override int HeapCapacity { get; set; } = 0;
+        /// <summary>
+        /// Returns actual length of Heap
+        /// </summary>
+        public override int HeapLength { get; set; } = 0;
+        protected override T[] heap { get; set; }
+        public override bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Heapify(int index)
+        {
+            if (index > HeapLength)
+                return;
+            int parent = GetParent(index);
+            //Parent node exists
+            if (parent >= 0 && parent != index)
+            {
+                //Parent node value is greated than child node
+                //1 preceeded 2 returns -1
+                if (this.heap[parent].CompareTo(this.heap[index]) > 0)
+                {
+                    Swap(parent, index);
+                    Heapify(parent);
+                }
+            }
+            int leftChild = LeftChild(index);
+            int rightChild = RightChild(index);
+            int smallestChild;
+            if (leftChild >= this.HeapLength)
+                return;
+            if (rightChild >= this.HeapLength)
+                smallestChild = leftChild;
+            else
+                smallestChild = this.heap[leftChild].CompareTo(this.heap[rightChild]) >= 0 ? leftChild : rightChild;
+            
+            if(this.heap[index].CompareTo(this.heap[smallestChild]) >= 0)
+            {
+                Swap(smallestChild, index);
+                Heapify(smallestChild);
+            }
+        }
+    }
+    public class MaxHeap<T> : Heap<T> where T : IComparable
+    {
+        public override int HeapCapacity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override int HeapLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        protected override T[] heap { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Heapify(int index)
+        {
+            throw new NotImplementedException();
+        }
     }
 
+    public abstract class Heap<T> : IHeap<T> , IEnumerable<T>
+    {
+        public Heap()
+        {
+            this.HeapCapacity = 64;
+            this.heap = new T[this.HeapCapacity];
+        }
+        public abstract int HeapCapacity { get; set; }
+        public abstract int HeapLength { get;  set; }
+        protected abstract T[] heap { get; set; }
+        public virtual void Swap(int x, int y)
+        {
+            T temp = this.heap[x];
+            this.heap[x] = this.heap[y];
+            this.heap[y] = temp;
+        }
+        public virtual int LeftChild(int index)
+        {
+            //2* index + 0 for 1 based heap
+            //For zero based Heap
+            return 2 *index +1;
+        }
+        /// <summary>
+        /// Get the Right Child
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public virtual int RightChild(int index)
+        {
+            //2*index +1 for 1 based Heap
+            //For zero based Heap
+            return 2 * index + 2;
+        }
+        public virtual int GetParent(int child)
+        {
+            // child/2 for 1 based Heap
+            //For 0 based Heap
+            return (child - 1) / 2;
+        }
+        protected abstract void Heapify(int index);
+        public virtual void Insert(T element)
+        {
+            this.heap[this.HeapLength] = element;
+            int temp = this.HeapLength;
+            this.HeapLength++;
+            Heapify(temp);
+        }
+
+        public virtual T Remove()
+        {
+            if (this.HeapLength <= 0)
+                throw new Exception("Heap is empty");
+            var elementRemove = this.heap[0];
+            this.heap[0] = this.heap[this.HeapLength - 1];
+            this.heap[this.HeapLength - 1] = default;
+            this.HeapLength--;
+            Heapify(this.HeapLength);
+            return elementRemove;
+        }
+
+        public virtual T Peek()
+        {
+            if (HeapLength > 0)
+                return heap[0];
+            else
+                return default;
+        }
+        public abstract bool Contains(T item);
+        public virtual void DeleteHeap()
+        {
+            this.heap = null;
+            this.HeapLength = default;
+            this.HeapCapacity = default;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            T[] arr = new T[this.HeapLength];
+            for(int i = 0; i < this.HeapLength; i++)
+            {
+                arr[i] = this.heap[i];
+            }
+            return arr.OfType<T>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface IHeap<T>
+    {
+        int HeapLength { get; set; }
+        void Swap(int x, int y);
+        int LeftChild(int index);
+        int RightChild(int index);
+        void Insert(T element);
+        T Remove();
+        T Peek();
+        bool Contains(T item);
+        void DeleteHeap();
+    }
 }
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -2,6 +2,7 @@
 using Arrays;
 using System.Collections.Generic;
 using LinkedList;
+using BinaryHeap;
 
 namespace DataStructure
 {
@@ -9,10 +10,23 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            PriorityQueue
+
+            
+            MinHeap<int> minHeap = new();
+            minHeap.Insert(1);
+            minHeap.Insert(10);
+            minHeap.Insert(100);
+            minHeap.Insert(2);
+            minHeap.Insert(3);
+            minHeap.Insert(5);
+            minHeap.Insert(6);
+            foreach (var min in minHeap)
+            {
+                Console.WriteLine(min);
+            }
+            Console.WriteLine(100.CompareTo(2));
             Console.WriteLine("Hello World!");
             Console.ReadKey();
-
         }
         static void DoublyLinkedList()
         {
@@ -36,6 +50,22 @@ namespace DataStructure
                 Console.WriteLine(da);
             }
             integerlist = null;
+        }
+        private class Person : IComparable
+        {
+            public string firstName;
+            public string lastName;
+       
+            public int CompareTo(object obj)
+            {
+                return this.firstName.CompareTo(((Person)obj).firstName);
+            }
+
+            public override int GetHashCode()
+            {
+                return firstName.GetHashCode();
+            }
+
         }
     }
 }
